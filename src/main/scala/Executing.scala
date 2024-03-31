323015200a9a5493a0522134da742b9becde4a85
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 23:01:28 +0100

    Update src/main/scala/Executing.scala

--- src/main/scala/Executing.scala
+++ src/main/scala/Executing.scala
@@ -9,9 +9,8 @@ import java.nio.file.Paths
 class Executing(spark: SparkSession) {
 
   private val environmentPaths = new EnvironmentPaths()
-  private val environmentDirectories = new EnvironmentDirectories()
 
-  def executions(): Unit = {
+  def executing(): Unit = {
 
     val urlString = "https://download.companieshouse.gov.uk/BasicCompanyData-2024-03-01-part1_7.zip"
     val name = urlString.split("/").last.trim
