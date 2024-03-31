ba57d00de2c9398b96217a143d1ff9150ba86f9c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 23:01:45 +0100

    Update src/main/scala/ExperimentsApp.scala

--- src/main/scala/ExperimentsApp.scala
+++ src/main/scala/ExperimentsApp.scala
@@ -1,14 +1,16 @@
 package pattern.intelligence
 
+import environment.{EnvironmentDirectories, EnvironmentPaths}
+
 import org.apache.logging.log4j.{Level, LogManager}
 import org.apache.spark.sql.SparkSession
-import pattern.intelligence.environment.{EnvironmentDirectories, EnvironmentPaths}
 
 
 
 object ExperimentsApp {
 
-  val environmentPaths = new EnvironmentPaths()
+  private val environmentPaths = new EnvironmentPaths()
+  private val environmentDirectories = new EnvironmentDirectories()
 
   def main(args: Array[String]): Unit = {
 
@@ -27,24 +29,20 @@ object ExperimentsApp {
     // Spark Logs
     spark.sparkContext.setLogLevel("ERROR")
 
-    /* Configuring for computation
-     *
-     * java.lang.Runtime.getRuntime.availableProcessors() deduces the number of machine threads
-     * spark.sql.shuffle.partitions: The number of shuffle partitions for joins & aggregation
-     * spark.default.parallelism: The default number of partitions delivered after a transformation
-     * spark.speculation: If true "performs speculative execution of tasks. This means if one or more tasks are
-     *                             running slowly in a stage, they will be re-launched."
-     */
+    // Configuring
     val threads: Int = scala.math.ceil(java.lang.Runtime.getRuntime.availableProcessors()).toInt
     val limit: Int = math.floor(threads / 2).toInt
     spark.conf.set("spark.sql.shuffle.partitions", limit.toString)
     spark.conf.set("spark.default.parallelism", limit.toString)
 
-
-    val environmentDirectories = new EnvironmentDirectories()
+    // Directories
     List(environmentPaths.coded, environmentPaths.decoded).foreach { pathString: String =>
       environmentDirectories.reset(directoryName = pathString)
     }
+
+    // Computations
+    val executing = new Executing(spark = spark)
+    executing.executing()
 
   }
 
