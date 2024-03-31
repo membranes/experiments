e3628000fc791e12060fdcb65da5f0d6977c677f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 21:44:56 +0100

    Update src/main/scala/Executions.scala: In Progress

--- src/main/scala/Executions.scala
+++ src/main/scala/Executions.scala
@@ -1,10 +1,23 @@
 package pattern.intelligence
 
 import org.apache.spark.sql.SparkSession
+import functions.FetchDataURL
+import environment.{EnvironmentDirectories, EnvironmentPaths}
+
+import java.nio.file.Paths
 
 class Executions(spark: SparkSession) {
 
+  private val environmentPaths = new EnvironmentPaths()
+  val environmentDirectories = new EnvironmentDirectories()
+
   def executions(): Unit = {
+
+
+
+    val fetchDataURL = new FetchDataURL()
+    fetchDataURL.fetchDataURL(urlString = "",
+      pathString = Paths.get(environmentPaths.dataDirectory, "").toString)
 
   }
 
