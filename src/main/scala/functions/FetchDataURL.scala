616bd08239f9e6e2b7f0adb10a8823e8ccfa8938
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 19:55:15 +0100

    Add src/main/scala/functions/FetchDataURL.scala

---
+++ src/main/scala/functions/FetchDataURL.scala
@@ -0,0 +1,26 @@
+package pattern.intelligence
+package functions
+
+import org.apache.commons.io.FileUtils
+
+import java.io.File
+import java.net.URL
+import scala.util.control.Exception
+
+/**
+ *
+ */
+class FetchDataURL {
+
+  /**
+   *
+   * @param urlString: The uniform resource locator of a data set
+   * @param pathString: The {[temporary] local depository} + {name} + {extension} for the fetched data set.
+   */
+  def fetchDataURL(urlString: String, pathString: String): Unit = {
+    Exception.allCatch.withTry(
+      FileUtils.copyURLToFile(new URL(urlString), new File(pathString))
+    )
+  }
+
+}
