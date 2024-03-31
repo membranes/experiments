118edfb517f06a6de39c108d4250a648d768ae08
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 19:55:36 +0100

    Add src/main/scala/functions/ScalaDataType.scala

---
+++ src/main/scala/functions/ScalaDataType.scala
@@ -0,0 +1,31 @@
+package pattern.intelligence
+package functions
+
+import org.apache.spark.sql.types.*
+
+class ScalaDataType {
+
+  def scalaDataType(dataTypeOfVariable: DataType): Unit = {
+
+    dataTypeOfVariable match {
+      case _: ByteType => "Byte"
+      case _: ShortType => "Short"
+      case _: IntegerType => "Int"
+      case _: LongType => "Long"
+      case _: FloatType => "Float"
+      case _: DoubleType => "Double"
+      case _: DecimalType => "java.math.BigDecimal"
+      case _: StringType => "String"
+      case _: BinaryType => "Array[Byte]"
+      case _: BooleanType => "Boolean"
+      case _: TimestampType => "java.sql.Timestamp" // Beware
+      case _: DateType => "java.sql.Date"
+      case _: ArrayType => "scala.collection.Seq"
+      case _: StructType => "org.apache.spark.sql.Row"
+      case _: MapType => "scala.collection.Map"
+      case _ => "string"
+    }
+
+  }
+
+}
