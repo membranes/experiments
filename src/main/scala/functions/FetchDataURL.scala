757b51f29eaa95f755e90da53b55ac860fec5ea2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-31 21:45:31 +0100

    Update src/main/scala/functions/FetchDataURL.scala: In Progress

--- src/main/scala/functions/FetchDataURL.scala
+++ src/main/scala/functions/FetchDataURL.scala
@@ -5,6 +5,7 @@ import org.apache.commons.io.FileUtils
 
 import java.io.File
 import java.net.URL
+import scala.util.Try
 import scala.util.control.Exception
 
 /**
@@ -17,10 +18,18 @@ class FetchDataURL {
    * @param urlString: The uniform resource locator of a data set
    * @param pathString: The {[temporary] local depository} + {name} + {extension} for the fetched data set.
    */
-  def fetchDataURL(urlString: String, pathString: String): Unit = {
-    Exception.allCatch.withTry(
+  def fetchDataURL(urlString: String, pathString: String): Boolean = {
+
+    val fetch: Try[Unit] = Exception.allCatch.withTry(
       FileUtils.copyURLToFile(new URL(urlString), new File(pathString))
     )
+
+    if (fetch.isFailure) {
+      sys.error(fetch.failed.get.getMessage)
+    } else {
+      true
+    }
+
   }
 
 }
