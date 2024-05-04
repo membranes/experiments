e82b3cca6a742fa6cbbab3bd68c1ff0ebf5d5a14
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-05 00:49:35 +0100

    Update src/main/scala/s3/Bucket.scala

--- src/main/scala/s3/Bucket.scala
+++ src/main/scala/s3/Bucket.scala
@@ -2,7 +2,8 @@ package pattern.intelligence
 package s3
 
 import scala.util.control.Exception
-import com.amazonaws.services.s3.{AmazonS3, model}
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.{CreateBucketRequest, CreateBucketResponse, HeadBucketRequest, HeadBucketResponse}
 
 import scala.util.Try
 
@@ -10,25 +11,53 @@ import scala.util.Try
  *
  * @param s3Client: The Amazon S3 Client authentication object
  */
-class Bucket(s3Client: AmazonS3) {
+class Bucket(s3Client: S3Client) {
 
   /**
    *
-   * @param bucketName: The name of the Amazon S3 bucket of interest
+   * @param bucketName: The name of a Amazon S3 bucket of interest
    */
   def createBucket(bucketName: String): Unit = {
 
-    if (s3Client.doesBucketExistV2(bucketName)) {
+    if (isBucket(bucketName = bucketName)) {
+
       println("The bucket %s exists.", bucketName)
+
     } else {
 
-      val isExecuted: Try[model.Bucket] = Exception.allCatch.withTry(s3Client.createBucket(bucketName))
-      if (isExecuted.isFailure) {
-        sys.error(isExecuted.failed.get.getMessage)
+      val bucketRequest: CreateBucketRequest = CreateBucketRequest.builder()
+        .bucket(bucketName).build()
+
+      val exc: Try[CreateBucketResponse] = Exception.allCatch.withTry(
+        s3Client.createBucket(bucketRequest))
+
+      if (exc.isFailure) {
+        sys.error(exc.failed.get.getMessage)
+      } else {
+        println("The bucket %s has been created.", bucketName)
       }
 
     }
 
   }
+
+  /**
+   *
+   * @param bucketName: The name of a Amazon S3 bucket of interest
+   * @return
+   */
+  private def isBucket(bucketName: String): Boolean = {
+
+    val headBucketRequest = HeadBucketRequest.builder().bucket(bucketName).build()
+    val exc: Try[HeadBucketResponse] = Exception.allCatch.withTry(
+      s3Client.headBucket(headBucketRequest)
+    )
+    exc.isSuccess
+
+  }
+
+
+
+
 
 }
