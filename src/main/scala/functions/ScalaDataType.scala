f5f6101483720bb01fb5e47b180822d6cfea841a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-04 19:58:19 +0100

    Update src/main/scala/functions/ScalaDataType.scala

--- src/main/scala/functions/ScalaDataType.scala
+++ src/main/scala/functions/ScalaDataType.scala
@@ -8,22 +8,22 @@ class ScalaDataType {
   def scalaDataType(dataTypeOfVariable: DataType): Unit = {
 
     dataTypeOfVariable match {
-      case _: ByteType => "Byte"
-      case _: ShortType => "Short"
-      case _: IntegerType => "Int"
-      case _: LongType => "Long"
-      case _: FloatType => "Float"
-      case _: DoubleType => "Double"
-      case _: DecimalType => "java.math.BigDecimal"
-      case _: StringType => "String"
-      case _: BinaryType => "Array[Byte]"
-      case _: BooleanType => "Boolean"
-      case _: TimestampType => "java.sql.Timestamp" // Beware
-      case _: DateType => "java.sql.Date"
-      case _: ArrayType => "scala.collection.Seq"
-      case _: StructType => "org.apache.spark.sql.Row"
-      case _: MapType => "scala.collection.Map"
-      case _ => "string"
+      case _: ByteType => Byte
+      case _: ShortType => Short
+      case _: IntegerType => Int
+      case _: LongType => Long
+      case _: FloatType => Float
+      case _: DoubleType => Double
+      case _: DecimalType => scala.math.BigDecimal
+      case _: BinaryType => Byte
+      case _: BooleanType => Boolean
+      case _: TimestampType => TimestampType // Beware
+      case _: DateType => DateType
+      case _: ArrayType => scala.collection.Seq
+      case _: StructType => org.apache.spark.sql.Row
+      case _: MapType => scala.collection.Map
+      case _: StringType => StringType
+      case _ => StringType
     }
 
   }
