200f9d070572ed969e75452817528e942723125f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-05-05 00:49:17 +0100

    Update src/main/scala/Executing.scala

--- src/main/scala/Executing.scala
+++ src/main/scala/Executing.scala
@@ -4,8 +4,8 @@ import environment.{EnvironmentPaths, Temporary}
 import functions.FetchDataURL
 import s3.{Authentication, Bucket}
 
-import com.amazonaws.services.s3.AmazonS3
 import org.apache.spark.sql.SparkSession
+import software.amazon.awssdk.services.s3.S3Client
 
 import java.nio.file.Paths
 
@@ -26,7 +26,7 @@ class Executing(spark: SparkSession) {
       pathString = Paths.get(environmentPaths.coded, name).toString)
 
     // An illustration of Amazon S3 bucket creation
-    val s3Client: AmazonS3 = new Authentication().authentication()
+    val s3Client: S3Client = new Authentication().authentication()
     new Bucket(s3Client = s3Client).createBucket(bucketName = temporary.internal)
 
   }
